45e414102a475d7a00ea71eaaf222abad8226eaf
Tarun-saxena <tarunsaxena1712@gmail.com>
2025-02-15 23:52:50 +0530

    Removed unwanted folder from repo

--- basic/mini weather app.js
+++
@@ -1,7 +0,0 @@
-let weather= prompt('how is the weather outside ')
-if( weather== 'rainy' || weather =='Rainy'){
-    console.log ('carry an umbrela')
-}
-else{
-    console.log('wear your sunglasses')
-}
--- basic/script.js
+++
@@ -1,4 +0,0 @@
-const title = document.getElementById('title')
-console.log(title.innerText)
-title.innerText ='hellomorning'
-console.log(title.innerText)
